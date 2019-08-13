b8402b69c4a912f9e950269a05539bb17a94b734
Kaleb Klein <klein.jae@gmail.com>
2019-08-13 19:52:39 -0400

    Added an about command

---
+++ commands/about.go
@@ -0,0 +1,67 @@
+package commands
+
+import (
+	"fmt"
+	"persephone/lib"
+	"persephone/utils"
+	"strconv"
+
+	"github.com/andersfylling/disgord"
+	"github.com/andersfylling/snowflake/v3"
+	"github.com/pazuzu156/aurora"
+)
+
+// About command.
+type About struct {
+	Command Command
+	Version string
+}
+
+// InitAbout initialized the about command.
+func InitAbout() About {
+	return About{Init("about", "Gets information about the bot", []UsageItem{}), "0.0.1"}
+}
+
+// Register registers and runs the about command.
+func (c About) Register() *aurora.Command {
+	c.Command.CommandInterface.Run = func(ctx aurora.Context) {
+		config := lib.Config()
+		id, _ := strconv.Atoi(config.BotID)
+		bot, _ := ctx.Aurora.GetMember(ctx.Message.GuildID, snowflake.NewSnowflake(uint64(id)))
+
+		var roles []string
+		for _, r := range bot.Roles {
+			groles, _ := ctx.Aurora.GetGuildRoles(ctx.Message.GuildID)
+
+			for _, gr := range groles {
+				if gr.ID == r {
+					roles = append(roles, gr.Name)
+				}
+			}
+		}
+
+		ctx.Aurora.CreateMessage(ctx.Message.ChannelID, &disgord.CreateMessageParams{
+			Embed: &disgord.Embed{
+				Title:       "About Persephone",
+				Description: fmt.Sprintf("Persephone is a bot written in Go. Version v%s", c.Version),
+				Color:       0x7FFF00,
+				Fields: []*disgord.EmbedField{
+					{
+						Name:  "Name",
+						Value: fmt.Sprintf("%s#%s", bot.User.Username, bot.User.Discriminator),
+					},
+					{
+						Name:  "ID",
+						Value: bot.User.ID.String(),
+					},
+					{
+						Name:  "Roles",
+						Value: utils.JoinString(roles, ", "),
+					},
+				},
+			},
+		})
+	}
+
+	return c.Command.CommandInterface
+}
