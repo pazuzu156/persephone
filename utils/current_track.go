0803013020068dd045103c2f32976063e737fcc0
Kaleb Klein <klein.jae@gmail.com>
2019-08-13 01:01:23 -0400

    band command is complete!
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- utils/current_track.go
+++ utils/current_track.go
@@ -3,37 +3,14 @@ package utils
 import (
 	"errors"
 	"persephone/database"
+	"persephone/lib"
 
 	"github.com/andersfylling/disgord"
 	"github.com/pazuzu156/lastfm-go"
 )
 
-type Track struct {
-	NowPlaying string "xml:\"nowplaying,attr,omitempty\""
-	Artist     struct {
-		Name string "xml:\",chardata\""
-		Mbid string "xml:\"mbid,attr\""
-	} "xml:\"artist\""
-	Name       string "xml:\"name\""
-	Streamable string "xml:\"streamable\""
-	Mbid       string "xml:\"mbid\""
-	Album      struct {
-		Name string "xml:\",chardata\""
-		Mbid string "xml:\"mbid,attr\""
-	} "xml:\"album\""
-	Url    string "xml:\"url\""
-	Images []struct {
-		Size string "xml:\"size,attr\""
-		Url  string "xml:\",chardata\""
-	} "xml:\"image\""
-	Date struct {
-		Uts  string "xml:\"uts,attr\""
-		Date string "xml:\",chardata\""
-	} "xml:\"date\""
-}
-
 // GetNowPlayingTrack returns the currently playing track
-func GetNowPlayingTrack(author *disgord.User, lfm *lastfm.Api) (Track, error) {
+func GetNowPlayingTrack(author *disgord.User, lfm *lastfm.Api) (lib.Track, error) {
 	tracks, err := GetRecentTracks(author, lfm, "3")
 
 	if err == nil {
@@ -44,21 +21,21 @@ func GetNowPlayingTrack(author *disgord.User, lfm *lastfm.Api) (Track, error) {
 				return track, nil
 			}
 
-			return Track{}, errors.New("You're not currently listening to anything")
+			return lib.Track{}, errors.New("You're not currently listening to anything")
 		}
 	}
 
-	return Track{}, err
+	return lib.Track{}, err
 }
 
-func GetRecentTracks(author *disgord.User, lfm *lastfm.Api, limit string) ([]Track, error) {
+func GetRecentTracks(author *disgord.User, lfm *lastfm.Api, limit string) ([]lib.Track, error) {
 	if user := database.GetUser(author); user.Username != "" {
 		np, _ := lfm.User.GetRecentTracks(lastfm.P{
 			"user":  user.Lastfm,
 			"limit": limit,
 		})
 
-		var tracks = []Track{} // for the return
+		var tracks = []lib.Track{} // for the return
 
 		if len(np.Tracks) > 0 {
 			for _, track := range np.Tracks {
@@ -69,5 +46,5 @@ func GetRecentTracks(author *disgord.User, lfm *lastfm.Api, limit string) ([]Tra
 		}
 	}
 
-	return []Track{}, errors.New("You're not currently logged in with Last.fm")
+	return []lib.Track{}, errors.New("You're not currently logged in with Last.fm")
 }
