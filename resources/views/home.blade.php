18790cee4a3d1630ef9ce17e52d57f85c23676f7
Kaleb Klein <klein.jae@gmail.com>
2019-09-12 21:02:38 -0400

    Updated home page content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.main')
 
 @section('content')
-<p>Main Content Page</p>
+<p>Main Content Page. For now, tis just a placeholder. Cool stuff will be here at some point</p>
 @endsection
