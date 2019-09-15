f2b024d5c696c1506e241a79a567b848d8e48f07
Kaleb Klein <klein.jae@gmail.com>
2019-09-15 19:42:33 -0400

    Added no self-tasting

--- commands/about.go
+++ commands/about.go
@@ -16,7 +16,7 @@ type About struct {
 }
 
 // Version holds the bot's version number
-const Version string = "1.0.4"
+const Version string = "1.1.0"
 
 // InitAbout initialized the about command.
 func InitAbout() About {
