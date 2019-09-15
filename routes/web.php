ea49bf2e7d9bba0c1bd7b97608725b24e9bad310
Kaleb Klein <klein.jae@gmail.com>
2019-09-15 12:42:45 -0400

    route edit

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,6 @@ Route::name('auth.')->prefix('auth')->group(function ($route) {
         return redirect()->route('auth.discord.begin');
     })->name('login');
     $route->get('/logout', 'Auth\LoginController@getLogout')->name('logout');
-    // $route->get('/logout', function() {
-    //     Auth::logout();
-
-    //     return redirect()->route('home')->with(['alert' => 'success', 'message' => 'You have successfully logged out!']);
-    // })->name('logout');
 
     $route->get('/authenticate/{discordId}/{token}', 'Auth\LoginController@authenticateUserWithToken');
     $route->get('/continue', 'Auth\LoginController@beginAuthFlow');
