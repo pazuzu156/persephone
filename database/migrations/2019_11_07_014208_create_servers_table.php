c4940eff8ae6310572f62f3d6c64681d688925bf
Kaleb Klein <klein.jae@gmail.com>
2019-11-06 20:44:50 -0500

    Added new servers migration

---
+++ database/migrations/2019_11_07_014208_create_servers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('guild_id');
+            $table->unsignedBigInteger('log_channel_id');
+            $table->unsignedBigInteger('elevated_role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servers');
+    }
+}
