fed66eb7607cbb07ce06bda5598c7d169c4a3f80
Kaleb Klein <klein.jae@gmail.com>
2019-12-08 19:22:06 -0500

    Albums done
    The albums grid is done

--- commands/profile.go
+++ commands/profile.go
@@ -6,6 +6,7 @@ import (
 	"persephone/lib"
 
 	"github.com/andersfylling/disgord"
+	"github.com/cavaliercoder/grab"
 	"github.com/fogleman/gg"
 	"github.com/nfnt/resize"
 	"github.com/pazuzu156/atlas"
@@ -48,6 +49,7 @@ func (c Profile) Register() *atlas.Command {
 		artists, _ := c.Lastfm.User.GetTopArtists(lastfm.P{"user": lfmuser.Name, "period": "overall", "limit": "5"})
 
 		// TODO: Get Top Albums
+		albums, _ := c.Lastfm.User.GetTopAlbums(lastfm.P{"user": lfmuser.Name, "limit": "4", "period": "overall"})
 
 		// TODO: Get Top Tags
 
@@ -57,22 +59,22 @@ func (c Profile) Register() *atlas.Command {
 		dc.DrawImage(bg, 0, 0)
 
 		dc.SetRGBA(1, 1, 1, 0.2)
-		dc.DrawRectangle(0, 100, 1000, 72)
+		dc.DrawRectangle(0, 50, 1000, 72)
 		dc.Fill()
 
 		// Draw avatar and add username + scrobble count
-		dc.DrawImage(avr, 315, 100)
+		dc.DrawImage(avr, 315, 50)
 		dc.LoadFontFace(FontBold, 26)
 		dc.SetRGB(0.2, 0.2, 0.2)
-		dc.DrawString(ctx.Message.Author.Username+" ("+lfmuser.Name+")", 391, 131)
+		dc.DrawString(ctx.Message.Author.Username+" ("+lfmuser.Name+")", 391, 80)
 		dc.SetRGB(0.9, 0.9, 0.9)
-		dc.DrawString(ctx.Message.Author.Username+" ("+lfmuser.Name+")", 390, 130)
+		dc.DrawString(ctx.Message.Author.Username+" ("+lfmuser.Name+")", 390, 79)
 		// scrobble count
 		dc.LoadFontFace(FontRegular, 20)
 		dc.SetRGB(0.2, 0.2, 0.2)
-		dc.DrawString(fmt.Sprintf("%s scrobbles", lib.HumanNumber(lfmuser.PlayCount)), 391, 161)
+		dc.DrawString(fmt.Sprintf("%s scrobbles", lib.HumanNumber(lfmuser.PlayCount)), 391, 111)
 		dc.SetRGB(0.9, 0.9, 0.9)
-		dc.DrawString(fmt.Sprintf("%s scrobbles", lib.HumanNumber(lfmuser.PlayCount)), 390, 160)
+		dc.DrawString(fmt.Sprintf("%s scrobbles", lib.HumanNumber(lfmuser.PlayCount)), 390, 110)
 
 		// Draw white box that goes behind album art + draw album art
 		dc.SetRGBA(1, 1, 1, 0.2)
@@ -81,10 +83,11 @@ func (c Profile) Register() *atlas.Command {
 
 		dc.SetRGB(0.2, 0.2, 0.2)
 		dc.LoadFontFace(FontBold, 40)
-		dc.DrawString("Top Artists", 66, 96)
+		dc.DrawString("Top Artists", 66, 91)
 		dc.SetRGB(0.8, 0.8, 0.8)
-		dc.DrawString("Top Artists", 65, 95)
+		dc.DrawString("Top Artists", 65, 90)
 
+		// display top artist and the other top 4
 		aa := lib.GetArtistImage(artists.Artists[0])
 
 		if aa == nil {
@@ -95,37 +98,54 @@ func (c Profile) Register() *atlas.Command {
 		dc.DrawImage(aar, 55, 105)
 
 		dc.LoadFontFace(FontRegular, 25)
-		dc.SetRGB(0.2, 0.2, 0.2)
-		dc.DrawString(lib.ShortStr(artists.Artists[0].Name, 33), 61, 371)
-		dc.DrawString(fmt.Sprintf("%s plays", artists.Artists[0].PlayCount), 61, 401)
-
-		dc.SetRGB(0.9, 0.9, 0.9)
-		dc.DrawString(lib.ShortStr(artists.Artists[0].Name, 33), 60, 370)
-		dc.DrawString(fmt.Sprintf("%s plays", artists.Artists[0].PlayCount), 60, 400)
+		lib.DrawStringWithShadow(lib.ShortStr(artists.Artists[0].Name, 33), 60, 370, dc)
+		lib.DrawStringWithShadow(fmt.Sprintf("%s plays", artists.Artists[0].PlayCount), 60, 400, dc)
 
 		x := float64(61)
 		y := float64(445)
 
+		// loop and display artists
 		for n, artist := range artists.Artists {
 			if n > 0 {
 				dc.LoadFontFace(FontRegular, 20)
 
+				lib.DrawStringWithShadow(lib.ShortStr(artist.Name, 25), x, y, dc)
+				lib.DrawWrappedStringWithShadow(artist.PlayCount, 50, y, 0, 1, 240, 0, gg.AlignRight, dc)
+
 				dc.SetRGB(0.2, 0.2, 0.2)
-				dc.DrawString(lib.ShortStr(artist.Name, 25), x, y)
-				dc.DrawStringWrapped(artist.PlayCount, 50, y, 0, 1, 240, 0, gg.AlignRight)
 				dc.SetLineWidth(0.5)
 				dc.DrawLine(x, y+5, x+230, y+5)
 				dc.Stroke()
-
 				dc.SetRGB(0.9, 0.9, 0.9)
-				dc.DrawString(lib.ShortStr(artist.Name, 25), x-1, y-1)
-				dc.DrawStringWrapped(artist.PlayCount, 49, y-1, 0, 1, 240, 0, gg.AlignRight)
-				dc.SetLineWidth(0.5)
 				dc.DrawLine(x-1, y+4, x+229, y+4)
 				dc.Stroke()
 
 				y = y + 40
 			}
+		}
+
+		dc.LoadFontFace(FontRegular, 20)
+		lib.DrawStringWithShadow("Top Albums", 475, 150, dc)
+
+		// display album grid.
+		for i, album := range albums.Albums {
+			ares, _ := grab.Get(lib.LocGet("temp/"), album.Images[3].URL)
+			ai, _ := lib.OpenImage(ares.Filename)
+			os.Remove(ares.Filename)
+			ar := resize.Resize(145, 145, ai, resize.Bicubic)
+			pos := AlbumPositions[i]
+
+			dc.SetRGBA(0, 0, 0, 0.3)
+			dc.DrawRoundedRectangle(pos.Shadow.X, pos.Shadow.Y, 155, 155, pos.Shadow.R)
+			dc.Fill()
+
+			dc.DrawImage(ar, pos.X, pos.Y)
+
+			dc.LoadFontFace(FontRegular, 20)
+			lib.DrawStringWithShadow(lib.ShortStr(album.Name, 15), pos.Info.X, pos.Info.Y, dc)
+
+			dc.LoadFontFace(FontRegular, 16)
+			lib.DrawStringWithShadow(fmt.Sprintf("%s plays", album.PlayCount), pos.Info.Plays.X, pos.Info.Plays.Y, dc)
 		}
 
 		lib.BrandImage(dc)
